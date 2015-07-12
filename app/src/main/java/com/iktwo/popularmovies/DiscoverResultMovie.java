ade2d05bd8795ef5118125bd2add4a2ef454870b
Iktwo Sh <iktwo.sh@gmail.com>
2015-07-12 01:02:51 -0700

    Make DiscoverResultMoview parcelable

--- app/src/main/java/com/iktwo/popularmovies/DiscoverResultMovie.java
+++ app/src/main/java/com/iktwo/popularmovies/DiscoverResultMovie.java
@@ -1,9 +1,21 @@
 package com.iktwo.popularmovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class DiscoverResultMovie {
+public class DiscoverResultMovie implements Parcelable {
+    public static final Creator<DiscoverResultMovie> CREATOR = new Creator<DiscoverResultMovie>() {
+        public DiscoverResultMovie createFromParcel(Parcel source) {
+            return new DiscoverResultMovie(source);
+        }
+
+        public DiscoverResultMovie[] newArray(int size) {
+            return new DiscoverResultMovie[size];
+        }
+    };
     public boolean adult;
     public String backdrop_path;
     public List<Integer> genre_ids = new ArrayList<>();
@@ -17,4 +29,43 @@ public class DiscoverResultMovie {
     public boolean video;
     public double vote_average;
     public double vote_count;
+
+    protected DiscoverResultMovie(Parcel in) {
+        this.adult = in.readByte() != 0;
+        this.backdrop_path = in.readString();
+        this.genre_ids = new ArrayList<Integer>();
+        in.readList(this.genre_ids, List.class.getClassLoader());
+        this.id = in.readDouble();
+        this.original_language = in.readString();
+        this.original_title = in.readString();
+        this.overview = in.readString();
+        this.poster_path = in.readString();
+        this.release_date = in.readString();
+        this.title = in.readString();
+        this.video = in.readByte() != 0;
+        this.vote_average = in.readDouble();
+        this.vote_count = in.readDouble();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeByte(adult ? (byte) 1 : (byte) 0);
+        dest.writeString(this.backdrop_path);
+        dest.writeList(this.genre_ids);
+        dest.writeDouble(this.id);
+        dest.writeString(this.original_language);
+        dest.writeString(this.original_title);
+        dest.writeString(this.overview);
+        dest.writeString(this.poster_path);
+        dest.writeString(this.release_date);
+        dest.writeString(this.title);
+        dest.writeByte(video ? (byte) 1 : (byte) 0);
+        dest.writeDouble(this.vote_average);
+        dest.writeDouble(this.vote_count);
+    }
 }
